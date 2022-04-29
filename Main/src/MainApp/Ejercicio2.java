c03333f03f33ca877bcc3273f2f94a196b1b59c8
mednologic <mednologic@gmail.com>
2022-04-29 15:32:04 +0200

    "More Updates"
    Tuplas operativas

--- Main/src/MainApp/Ejercicio2.java
+++ Main/src/MainApp/Ejercicio2.java
@@ -14,14 +14,14 @@ public class Ejercicio2 {
 	public static void ejecutarEjercicio2(Connection conn) throws SQLException {
 		SqlTools.createDB("PruebaJAVA2", conn);
 		//Creamos una List donde almacenar las querys que necesitamos 
-		List<String> querys = new ArrayList<String>();
-		querys.add("CREATE TABLE departamentos ("+
+		List<String> querysCreating = new ArrayList<String>();
+		querysCreating.add("CREATE TABLE departamentos ("+
 				  "CODIGO int NOT NULL,"+
 				  "NOMBRE varchar(255) NOT NULL,"+
 				  "PRESUPUESTO decimal(10,0) NOT NULL,"+
 				  "PRIMARY KEY (CODIGO)"+
 				") ENGINE=InnoDB DEFAULT CHARSET=utf8;");
-		querys.add("CREATE TABLE `empleados` ("+
+		querysCreating.add("CREATE TABLE `empleados` ("+
 				  "`DNI` int NOT NULL,"+
 				  "`NOMBRE` varchar(255) NOT NULL,"+
 				  "`APELLIDOS` varchar(255) NOT NULL,"+
@@ -31,10 +31,17 @@ public class Ejercicio2 {
 				  "CONSTRAINT `empleados_ibfk_1` FOREIGN KEY (`DEPARTAMENTO`) REFERENCES `departamentos` (`CODIGO`)"+
 				") ENGINE=InnoDB DEFAULT CHARSET=utf8;");
 		
-		for (String str : querys) {
+		
+		for (String str : querysCreating) {
 			SqlTools.createTable(str, conn);
 		}
-
+		String queryAddTuple = "INSERT INTO `departamentos` VALUES "
+				+ "(14,'IT',65000),"
+				+ "(37,'Accounting',15000)"
+				+ ",(59,'Human Resources',240000)"
+				+ ",(77,'Research',55000);";
+		SqlTools.createTuple(queryAddTuple, conn);
+		
 	}
 
 }
