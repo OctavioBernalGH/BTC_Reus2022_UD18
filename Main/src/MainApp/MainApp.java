fde1224198c55293549999c418a1fdcf2230008b
OctavioBernalGH <103035621+OctavioBernalGH@users.noreply.github.com>
2022-04-29 13:06:58 +0200

    test

--- Main/src/MainApp/MainApp.java
+++ Main/src/MainApp/MainApp.java
@@ -1,5 +1,8 @@
 package MainApp;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.swing.JOptionPane;
 
 import SqlTools.SqlTools;
@@ -7,19 +10,41 @@ import SqlTools.SqlTools;
 
 public class MainApp {
 	
-	
-
+		
 	public static void main(String[] args) {
+		
 		String address = JOptionPane.showInputDialog("Direccion del servidor");
 		String userSQL = JOptionPane.showInputDialog("Nombre del usuario");
 		String password= JOptionPane.showInputDialog("Password");
-	
+		
+
+		SqlTools herramientasSQL = new SqlTools(address, userSQL, password);
+		
+		String Query;
+		String name;
+		
 		try {
 			SqlTools.createConnection(address, userSQL, password);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
+	
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
 
+
+			try {
+			name = JOptionPane.showInputDialog("Introduce número de DDBB a crear:");
+			Query = "CREATE DATABASE " + name;
+			Statement st = SqlTools.crearStatement(null);
+			st.execute(Query);
+			JOptionPane.showMessageDialog(null, "Se ha creado la BBDD " + name);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println(e);
+		}
+		
+		
 	}
 
 }
--- Main/src/SqlTools/SqlTools.java
+++ Main/src/SqlTools/SqlTools.java
@@ -3,18 +3,32 @@ package SqlTools;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class SqlTools {
 	
+	protected String adressSQL;
+	protected String userNameSQL;
+	protected String passwordSQL;
+	
+	public SqlTools() {}
+	
+	public SqlTools(String adressSQL, String userNameSQL, String passwordSQL) {
+		this.adressSQL = adressSQL;
+		this.userNameSQL = userNameSQL;
+		this.passwordSQL = passwordSQL;
+	}
+	
 	static Connection mysqlConn = null;
 	
-	public static Connection createConnection(String address, String userMysql, String passwordMysql) throws ClassNotFoundException {
+	// Método para crear la conexión
+	public static Connection createConnection(String addressSQL, String userNameSQL, String passwordSQL) throws ClassNotFoundException {
 		
 		try {
 			
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			mysqlConn =DriverManager.getConnection("jdbc:mysql://"+address+":3306?useTimezone=UTC",userMysql,passwordMysql);	
-			System.out.println("Conectado");
+			mysqlConn =DriverManager.getConnection("jdbc:mysql://"+addressSQL+":3306?useTimezone=UTC",userNameSQL,passwordSQL);	
+			System.out.println("Conexión establecida con el servidor.");
 			
 		}catch(SQLException | ClassNotFoundException e){
 			System.out.println("No se ha podido connectar a la base de datos");
@@ -23,12 +37,29 @@ public class SqlTools {
 		
 		return mysqlConn;
 	}
-	public void closeConnection() {
+	
+	// Método para crear el Statement de la conexión.
+	public static Statement crearStatement(Connection mysqlConn) {
+		
+		try {
+			Statement st = mysqlConn.createStatement();
+			return st;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+		
+	}
+	
+	// Método para cerrar la conexión
+	public static Connection closeConnection() {
 		try {
 			mysqlConn.close();
 		}catch(SQLException e) {
 			System.out.println(e);
 		}
+		return mysqlConn;
 	}
 
 }
