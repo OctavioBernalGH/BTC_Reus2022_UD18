0f9c90fe13017587f6b8e8cee5464d0fe67cc090
OctavioBernalGH <103035621+OctavioBernalGH@users.noreply.github.com>
2022-04-29 17:13:41 +0200

    Se añaden funcionalidades.
    Se añaden funcionalidades.

--- Main/src/MainApp/Ejercicio_1.java
+++ Main/src/MainApp/Ejercicio_1.java
@@ -7,6 +7,8 @@ package MainApp;
  */
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JOptionPane;
 
@@ -23,16 +25,36 @@ public class Ejercicio_1 {
 			// Menú de opciones mediante showOptionalDialog
 			int seleccionFuncion = JOptionPane.showOptionDialog(null, "Seleccione opcion", "Selector de opciones",
 					JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null,
-					new Object[] { "Crear BBDD", "Crear Tabla", "Realizar insert", "Exit" }, " 1");
+					new Object[] { "Realizar ejercicio 1", "Exit" }, " 1");
 
 			// Selector de opción y acción a realizar.
 			if (seleccionFuncion == 0) {
-				SqlTools.createDB("Test", conn);
+
+				SqlTools.createDB("UD18_Ejercicio_1", conn);
+				// Definimos una lista para llenarla de querys.
+				List<String> querysCreating = new ArrayList<String>();
+
+				// Definimos las querys de creación de tablas
+				querysCreating.add("CREATE TABLE departamentos (" + "CODIGO int NOT NULL,"
+						+ "NOMBRE varchar(255) NOT NULL," + "PRESUPUESTO decimal(10,0) NOT NULL,"
+						+ "PRIMARY KEY (CODIGO)" + ") ENGINE=InnoDB DEFAULT CHARSET=utf8;");
+				querysCreating.add("CREATE TABLE `empleados` (" + "`DNI` int NOT NULL,"
+						+ "`NOMBRE` varchar(255) NOT NULL," + "`APELLIDOS` varchar(255) NOT NULL,"
+						+ "`DEPARTAMENTO` int NOT NULL," + "PRIMARY KEY (`DNI`),"
+						+ "KEY `DEPARTAMENTO` (`DEPARTAMENTO`),"
+						+ "CONSTRAINT `empleados_ibfk_1` FOREIGN KEY (`DEPARTAMENTO`) REFERENCES `departamentos` (`CODIGO`)"
+						+ ") ENGINE=InnoDB DEFAULT CHARSET=utf8;");
+				// Recorrer las query y ejecutarlas
+				for (String str : querysCreating) {
+					SqlTools.createTable(str, conn);
+				}
+
+				String queryAddTuple = "INSERT INTO `departamentos` VALUES " + "(14,'IT',65000),"
+						+ "(37,'Accounting',15000)" + ",(59,'Human Resources',240000)" + ",(77,'Research',55000);";
+				//Creamos las tuplas en la base de datos
+				SqlTools.createTuple(queryAddTuple, conn);
+				
 			} else if (seleccionFuncion == 1) {
-				SqlTools.createDB("Test", conn);
-			} else if (seleccionFuncion == 2) {
-				SqlTools.createDB("Test", conn);
-			} else if (seleccionFuncion == 3) {
 				JOptionPane.showMessageDialog(null, "Se ha salido con exito");
 				break;
 			}
