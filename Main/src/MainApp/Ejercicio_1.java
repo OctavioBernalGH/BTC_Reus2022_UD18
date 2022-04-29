998af74ba7030179960128bdb24fa83dafbff520
OctavioBernalGH <103035621+OctavioBernalGH@users.noreply.github.com>
2022-04-29 14:21:55 +0200

    Se añaden comentarios en las clases.
    Se añade author, date y version , se crean comentarios descriptivos de las funcionalidades y se reorganiza el código.

--- Main/src/MainApp/Ejercicio_1.java
+++ Main/src/MainApp/Ejercicio_1.java
@@ -1,5 +1,10 @@
 package MainApp;
 
+/**
+ * @author Octavio, David, Uri
+ * @date 29/04/2022
+ * @version 0.0.1
+ */
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -10,25 +15,27 @@ import SqlTools.SqlTools;
 public class Ejercicio_1 {
 
 	public static void ejecutarEjercicio1(Connection conn) throws SQLException {
+
+		// Bucle infinito para el menú
 		int bucle = 0;
 		while (bucle == 0) {
-			
-	
-		int seleccionFuncion = JOptionPane.showOptionDialog(null, "Seleccione opcion", "Selector de opciones",
-				JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null,
-				new Object[] { "Crear BBDD", "Crear Tabla", "Realizar insert", "Exit" }, " 1");
-		
-		
-		if (seleccionFuncion == 0) {
-			SqlTools.createDB("Test", conn);
-		} else if (seleccionFuncion == 1) {
-			SqlTools.createDB("Test", conn);
-		} else if (seleccionFuncion == 2) {
-			SqlTools.createDB("Test", conn);
-		} else if (seleccionFuncion == 3) {
-			JOptionPane.showMessageDialog(null, "Se ha salido con exito");
-			break;
-		} 
+
+			// Menú de opciones mediante showOptionalDialog
+			int seleccionFuncion = JOptionPane.showOptionDialog(null, "Seleccione opcion", "Selector de opciones",
+					JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null,
+					new Object[] { "Crear BBDD", "Crear Tabla", "Realizar insert", "Exit" }, " 1");
+
+			// Selector de opción y acción a realizar.
+			if (seleccionFuncion == 0) {
+				SqlTools.createDB("Test", conn);
+			} else if (seleccionFuncion == 1) {
+				SqlTools.createDB("Test", conn);
+			} else if (seleccionFuncion == 2) {
+				SqlTools.createDB("Test", conn);
+			} else if (seleccionFuncion == 3) {
+				JOptionPane.showMessageDialog(null, "Se ha salido con exito");
+				break;
+			}
 		}
 	}
 }
--- Main/src/MainApp/MainApp.java
+++ Main/src/MainApp/MainApp.java
@@ -1,5 +1,10 @@
 package MainApp;
 
+/**
+ * @author Octavio, David, Uri
+ * @date 29/04/2022
+ * @version 0.0.1
+ */
 import java.sql.SQLException;
 
 import View.ViewSelector;
