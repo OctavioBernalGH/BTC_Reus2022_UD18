130b1f45dcb1df709991859c3cce389f669ed3a9
OctavioBernalGH <103035621+OctavioBernalGH@users.noreply.github.com>
2022-04-29 14:33:30 +0200

    Se corrige bug en SqlTools.

--- Main/src/MainApp/MainApp.java
+++ Main/src/MainApp/MainApp.java
@@ -1,25 +1,20 @@
 package MainApp;
 
-UD18_Ejercicio_1
+
 /**
  * @author Octavio, David, Uri
  * @date 29/04/2022
  * @version 0.0.1
  */
 import java.sql.SQLException;
-
 import java.sql.SQLException;
 import java.sql.Statement;
-
 import javax.swing.JOptionPane;
-
 import SqlTools.SqlTools;
-UD18_Testing_1
-
 import View.ViewSelector;
 
 public class MainApp {
-UD18_Ejercicio_1
+	
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
