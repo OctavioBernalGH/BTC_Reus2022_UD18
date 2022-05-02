134e0ab38dea1d9c3773a338b1108d633e7049e8
DavidDalmauDieguez <david.dieguez13@gmail.com>
2022-05-02 10:29:23 +0200

    random commit

--- Main/src/MainApp/MainApp.java
+++ Main/src/MainApp/MainApp.java
@@ -9,9 +9,6 @@ import View.ViewSelector;
 
 
 public class MainApp {
-	
-	
-
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		ViewSelector.selectorVistas();
 
